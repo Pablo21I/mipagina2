c81641e25721c3a46550332b48ff4819cde4238e
pablo <navarropablo6999@gmail.com>
2026-03-23 16:32:46 -0600

    fix routes

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,11 +1,27 @@
-<?php $currentView = $currentView ?? ''; ?>
+<?php
+$currentView = $currentView ?? '';
+$docRoot = isset($_SERVER['DOCUMENT_ROOT']) ? str_replace('\\', '/', rtrim($_SERVER['DOCUMENT_ROOT'], '/')) : '';
+$projectRoot = str_replace('\\', '/', realpath(__DIR__ . '/..'));
+5$relativeRoot = '';
+
+if ($docRoot && strpos($projectRoot, $docRoot) === 0) {
+    $relativeRoot = substr($projectRoot, strlen($docRoot));
+}
+
+$basePath = '/' . trim($relativeRoot, '/');
+if ($basePath === '/') {
+    $basePath = '/';
+}
+
+$baseUrl = rtrim($basePath, '/') . '/';
+?>
         <aside class="sidebar">
             <ul>
-                <li><a class="<?php echo $currentView === 'dashboard' ? 'active' : ''; ?>" href="http://localhost/mipagina2/templates/plantilla.php?view=dashboard">Dashboard</a></li>
-                <li><a class="<?php echo $currentView === 'clientes' ? 'active' : ''; ?>" href="http://localhost/mipagina2/templates/plantilla.php?view=clientes">Clientes</a></li>
-                <li><a class="<?php echo $currentView === 'usuarios' ? 'active' : ''; ?>" href="http://localhost/mipagina2/templates/plantilla.php?view=usuarios">Usuarios</a></li>
-                <li><a href="reportes.php">Reportes</a></li>
-                <li><a href="configuracion.php">Configuración</a></li>
-                <li><a href="ayuda.php">Ayuda</a></li>
+                <li><a class="<?php echo $currentView === 'dashboard' ? 'active' : ''; ?>" href="<?php echo $baseUrl; ?>templates/plantilla.php?view=dashboard">Dashboard</a></li>
+                <li><a class="<?php echo $currentView === 'clientes' ? 'active' : ''; ?>" href="<?php echo $baseUrl; ?>templates/plantilla.php?view=clientes">Clientes</a></li>
+                <li><a class="<?php echo $currentView === 'usuarios' ? 'active' : ''; ?>" href="<?php echo $baseUrl; ?>templates/plantilla.php?view=usuarios">Usuarios</a></li>
+                <li><a href="<?php echo $baseUrl; ?>reportes.php">Reportes</a></li>
+                <li><a href="<?php echo $baseUrl; ?>configuracion.php">Configuración</a></li>
+                <li><a href="<?php echo $baseUrl; ?>ayuda.php">Ayuda</a></li>
             </ul>
         </aside>
